33ee2c0fee83684d45363c1d4dada9d58d99675d
tommymaher15 <tommymaher10@gmail.com>
2020-10-05 18:07:06 +0100

    Updated CSS Resets :lipstick:

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,12 +3,20 @@ import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
 
-  body {
-    box-sizing:border-box;
-    margin: 0;
-    padding: 0;
-    
-  };
+html {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  -webkit-box-sizing: inherit;
+  -moz-box-sizing: inherit;
+  box-sizing: inherit;
+  }
+  * {
+margin: 0;
+padding: 0;
+}
 
  `
 export default GlobalStyle;
