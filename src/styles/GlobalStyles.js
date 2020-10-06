5f124c897f31f652217f270c8c5832543e5945c1
tommymaher15 <tommymaher10@gmail.com>
2020-10-06 21:17:45 +0100

    some global style additions

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -7,6 +7,9 @@ html {
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
+  background-color:#f1f7fa;
+ 
+
 }
 *, *:before, *:after {
   -webkit-box-sizing: inherit;
@@ -18,5 +21,10 @@ margin: 0;
 padding: 0;
 }
 
+a { text-decoration: none;  }
+a:visited { text-decoration: none; }
+a:hover { text-decoration: none; }
+a:focus { text-decoration: none; }
+a:hover, a:active { text-decoration: none; }
  `
 export default GlobalStyle;
