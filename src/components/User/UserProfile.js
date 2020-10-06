1810d91f9eda85f276528a01d522cc91424aa101
tommymaher15 <tommymaher10@gmail.com>
2020-10-06 21:20:22 +0100

    Refined and Ready to go for feedback! :+1:

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import styled from "styled-components";
 import { GoOrganization, GoPerson, GoLocation} from "react-icons/go";
-
+import theme from '../../styles/theme';
 // pass the oprops down from the app component  
 const UserProfile = ({profile}) => {
 
-  // blank display until request is made. LETS GO 
+  
+
+
+
 if (!profile |profile.length === 0 ) return null;
 
     return (
         <Profile>
-
+   
       <h1>{profile.name} </h1>
-      <p><a href = {profile.url}>{profile.login}</a></p>
+      <p><a href = {profile.url}> {profile.login}</a></p>
 
       <ProfileImage src = {profile.avatar_url}  alt = "github User profile" loading = "lazy" /> 
-
+      <IconWrap>
      <FollowsWrapper>
-      <FollowItem><GoOrganization/>  {profile.followers} followers</FollowItem>
-      <FollowItem><GoPerson />  {profile.following} following</FollowItem>
-      
+      <FollowItem><GoOrganization color = "777763"/>  {profile.followers} followers</FollowItem>
+      <FollowItem><GoPerson color = "777763" />  {profile.following} following</FollowItem>
+    
+   
       </FollowsWrapper>
       <Location>
-        <GoLocation /> {profile.location}
+        <GoLocation color = "777763" /> {profile.location}
         </Location>
+</IconWrap>
         </Profile>
     )
 }
@@ -31,24 +36,52 @@ export default UserProfile
 
 
 const Profile = styled.div`
+
 display:flex; 
 align-items:center;
 justify-content:center;
 flex-direction:column;
+h1 {
+  font-family: Calibre;
+    font-weight: 600;
+  color:${theme.colors.slate};
+  font-size:3em;
+
+}
+
+p{
+  font-family: "SF Mono";
+    font-weight: 400;
+    font-size:20px;
+  color:#777763;
+  padding-bottom:1em;
+ a:visted{
+   color:#777763;
+ };
+
+ a:hover{
+
+  color:red;
+
+ }
+}
 
 
 
 `
 
 
+
 const ProfileImage = styled.img`
- position: relative;
-  width: 20vw;
-  height: 20vw;
+
+ width: 100%;
+  max-width: 350px;
+  height: auto;
   overflow: hidden;
+
   border-radius: 50%;
-  margin-top:2em;
-  border: 0.3rem solid #081d2c;
+
+  ;
 
 `;
 
@@ -64,4 +97,7 @@ padding:1em;
 `;
 
 const Location = styled.p`
-padding:2em;`;
+padding:2em;
+text-align:center;`;
+
+const IconWrap = styled.div``;
