98eccfe93d7481d5f75ef46e46d33a14450a185b
tommymaher15 <tommymaher10@gmail.com>
2020-10-06 22:59:08 +0100

    refined shadow + gradient

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -28,9 +28,9 @@ width: 100%;
   border-radius: 1.625rem;
   font-family: Calibre;
     font-weight: 400;
-background: linear-gradient(315deg, #d9dee1, #ffffff);
-box-shadow:  -5px -5px 5px #c8cdd0, 
-             5px 5px 5px #ffffff;
+    background: linear-gradient(315deg, #d9dee1, #ffffff);
+box-shadow:  -5px -5px 5px #e5ebee, 
+             5px 5px 5px #fdffff;
   padding: 0 3.5rem 0 1.5rem;
   font-size: 1rem;
   background-color:#f1f7fa
